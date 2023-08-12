e81e5c9a5bbc3f7166a5e9f331edb3150b39d0b6
wjfatuan <wilson.forero@uan.edu.co>
2023-08-11 19:44:09 -0500

    Refactor

--- app/src/main/java/co/edu/uan/android/biggernumber0811/MainActivity.kt
+++ app/src/main/java/co/edu/uan/android/biggernumber0811/MainActivity.kt
@@ -10,40 +10,39 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    var leftNumber: Int = Random.nextInt(0,100)
-    var rightNumber: Int = (0..100).random()
+    var leftNumber: Int = 0
+    var rightNumber: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // layout: un archivo xml que contiene la definicion del UI
 
+        asignarValoresAleatorios()
+
         val bl = findViewById<Button>(R.id.buttonLeft)
         val br = findViewById(R.id.buttonRight) as Button
 
         br.setOnClickListener {
-            if(rightNumber>leftNumber) {
-                Toast.makeText(this, "Has ganado!", Toast.LENGTH_LONG).show()
-                println("Has ganado!")
-            } else {
-                Toast.makeText(this, "Has perdido", Toast.LENGTH_LONG).show()
-                println("Has perdido :(")
-            }
-            leftNumber = Random.nextInt(0,100)
-            rightNumber = (0..100).random()
-
+            validarSeleccion(rightNumber, leftNumber)
         }
 
         bl.setOnClickListener {
-            if(rightNumber<leftNumber) {
-                Toast.makeText(this, "Has ganado!", Toast.LENGTH_LONG).show()
-                println("Has ganado!")
-            } else {
-                Toast.makeText(this, "Has perdido", Toast.LENGTH_LONG).show()
-                println("Has perdido :(")
-            }
-            leftNumber = Random.nextInt(0,100)
-            rightNumber = (0..100).random()
+            validarSeleccion(leftNumber, rightNumber)
         }
+    }
+
+    fun validarSeleccion(numeroGrande: Int, numeroPequeno: Int) {
+        println("numero grande: $numeroGrande, numero pequeno $numeroPequeno")
+        if(numeroGrande>numeroPequeno) {
+            Toast.makeText(this, "Has ganado!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Has perdido", Toast.LENGTH_LONG).show()
+        }
+        asignarValoresAleatorios()
+    }
+    fun asignarValoresAleatorios() {
+        leftNumber = Random.nextInt(0,100)
+        rightNumber = (0..100).random()
     }
 }
